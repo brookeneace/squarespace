c65d83eb33daa368c606e2843cf29545e77900de
brookeneace <brooke.neace@gmail.com>
2017-12-20 11:44:18 -0500

    added data attributes and hover event

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -11,6 +11,12 @@ angular.module('squarespaceApp')
 			console.log(response.data);
 			$scope.assets = response.data; 
 	});
+
+    
+    $scope.hoverAttr = function (e) {
+        var id = $(e.target).data('id');
+        console.log(id); // I want to get the data attribute of the element i am hovering 
+    };
 	
 
 })
