664284368eac46b25575c865a2213ab40d1279e4
brookeneace <brooke.neace@gmail.com>
2017-12-20 14:10:10 -0500

    css tweaks

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('squarespaceApp')
 .controller('mainCtrl', function($scope, dataService){
 
 	
-	$scope.helloConsole = dataService.helloConsole;
+// 	$scope.helloConsole = dataService.helloConsole;
 	
 	
 	dataService.getAssets(function(response){
@@ -16,6 +16,13 @@ angular.module('squarespaceApp')
     $scope.hoverAttr = function (e) {
         var id = $(e.target).data('id');
         console.log(id); // I want to get the data attribute of the element i am hovering 
+        $('.portfolio').find('.item' + "[data-id='" + id +"']").addClass('active');//find the image div with the same data id and add active class
+    };
+    
+     $scope.hoverRemove = function (e) {
+        var id = $(e.target).data('id');
+        console.log(id); // I want to get the data attribute of the element i am hovering 
+        $('.portfolio').find('.item' + "[data-id='" + id +"']").removeClass('active');//find the image div with the same data id and remove active class
     };
 	
 
